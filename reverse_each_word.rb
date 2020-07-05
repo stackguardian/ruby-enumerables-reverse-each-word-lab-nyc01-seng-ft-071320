8e1d4497814b6e0043cc6536c070f2487c1f8988
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-05 20:43:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,10 +8,3 @@ def reverse_each_word(string)
   reversed_array.join(" ")
 end
 
-def reverse_each_word(string)
-  new_array = string.split
-  new_array.collect do |words|
-    words.reverse 
-  end
-  new_array
-end
